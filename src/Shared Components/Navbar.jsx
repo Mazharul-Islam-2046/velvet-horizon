490bfee179a3a358bdb79bc2bc6362be12e48004
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2024-05-02 08:03:18 +0600

    velvet horizon Navbar, and initialization

---
+++ src/Shared Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+    return (
+        <div className="flex justify-between items-center px-16 py-8 absolute top-0 left-0 right-0">
+            <p className="font-bold text-xl">Velvet Horizon</p>
+            <div className="flex gap-5 justify-between items-center tracking-wider">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/Movies">Movies</NavLink>
+                <NavLink to="/tv-series">TV Series</NavLink>
+                <NavLink to="/animations">Animation</NavLink>
+                <NavLink to="/documentry">Documentry</NavLink>
+            
+            </div>
+            
+
+            <div className="flex gap-3 font-semibold">
+                <p className="px-5 py-2 border-gray-400 border-2 rounded-lg">Signup</p>
+                <p className="px-5 py-2 bg-green-700 rounded-lg">Login</p>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
